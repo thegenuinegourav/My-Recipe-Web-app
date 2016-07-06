4fa41d5cb800bbbe6ea4ac552d823e6cc8dfe58f
Gourav Suri <gourav96suri@gmail.com>
2016-07-06 09:21:56 +0000

    create test for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ActiveRecord::Base
-
+  
+  validates :name, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :summary, presence: true, length: { minimum: 10, maximum: 150 }
+  validates :description, presence: true, length: {minimum: 20,maximum:500 }
 end
---
+++ test/models/recipe_test.rb
@@ -0,0 +1,57 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+   
+   def setup
+      @recipe = Recipe.new(name: "Brownie", summary: "Best Chocolate Brownie ever especially for kids", description: "Add butter and curd, Beat it. Add flour and microwave it.") 
+   end
+   
+   test "recipe should be valid" do
+       assert @recipe.valid?
+   end
+   
+   test "name should be present" do
+       @recipe.name = " "
+       assert_not @recipe.valid?
+   end
+   
+   test "name should not be too short" do
+       @recipe.name = "aaaa"
+       assert_not @recipe.valid?
+   end
+   
+   test "name should not be too long" do
+       @recipe.name = "a" * 101
+       assert_not @recipe.valid?
+   end
+   
+   test "summary should be present" do
+       @recipe.summary = " "
+       assert_not @recipe.valid?
+   end
+   
+   test "summary should not be too short" do
+       @recipe.summary = "aaaaaaaaa"
+       assert_not @recipe.valid?
+   end
+   
+   test "summary should not be too long" do
+       @recipe.summary = "a" * 151
+       assert_not @recipe.valid?
+   end
+   
+   test "description should be present" do
+       @recipe.description=" "
+       assert_not @recipe.valid?
+   end
+   
+   test "description should not be too short" do
+       @recipe.description = "a" * 19
+       assert_not @recipe.valid?
+   end
+   
+   test "description should not be too long" do
+       @recipe.description = "a" * 501
+       assert_not @recipe.valid?
+   end
+end
