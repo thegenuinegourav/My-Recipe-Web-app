be03126581024975e2f92e812de9c262439537b4
Gourav Suri <gourav96suri@gmail.com>
2016-07-11 19:01:19 +0000

    Solved bug for deploy

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
     CarrierWave.configure do |config|
-        confif.fog_credentials = {
+        config.fog_credentials = {
             :provider => 'AWS',
             :aws_access_key_id => ENV['S3_ACCESS_KEY'],
             :aws_secret_access_key => ENV['S3_SECRET_KEY']
